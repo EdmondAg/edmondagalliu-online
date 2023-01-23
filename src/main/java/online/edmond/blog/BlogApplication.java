b93f34a498e4d84e94e90422054c1490c17e0f40
Edmond <97a.edmond@gmail.com>
2023-01-23 20:16:31 +0100

    CV changed

--- src/main/java/online/edmond/blog/BlogApplication.java
+++ src/main/java/online/edmond/blog/BlogApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApplication {
 
     public static void main(String[] args) {
+        int i = 4;
+        System.out.println(i);
         SpringApplication.run(BlogApplication.class, args);
     }
 
