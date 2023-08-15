d5c831600ab7ea7cce244f21fae7dfb0a0ab4bd2
edmond.agalliu <edmond.14a@gmail.com>
2023-08-15 13:17:51 +0200

    cv update

--- src/main/java/online/edmond/blog/repository/PersonsRepository.java
+++
@@ -1,8 +0,0 @@
-package online.edmond.blog.repository;
-
-import online.edmond.blog.model.Persons;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PersonsRepository extends JpaRepository<Persons, Long> {
-}
--- src/main/java/online/edmond/blog/service/PersonsService.java
+++
@@ -1,7 +0,0 @@
-package online.edmond.blog.service;
-
-import online.edmond.blog.model.Persons;
-
-public interface PersonsService {
-    Iterable<Persons> listAllPersons();
-}
